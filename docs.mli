5585c0f4a330d3651af23ef96385751383b40171
Raynos <raynos2@gmail.com>
2014-04-15 13:52:56 -0700

    Updated type definition for fixtures-fs

--- docs.mli
+++ docs.mli
@@ -1,13 +1,45 @@
+type FsMock := {
+    fs: {
+        writeFile: (String, String, Callback) => void,
+        mkdir: (String, Callback) => void,
+        unlink: (String, Callback) => void
+    },
+    mkdirp: (String, Callback) => void,
+    rimraf: (String, Callback) => void
+}
 
+type Fixture := Object<String, String | Fixture>
 
-create-fixtures := (
+type Task<T> := Callback<Error, T> | Object & {
+    end: Callback<Error, T>
+}
+
+fixtures-fs/create-fixtures := (
     dirname: String,
-    fixtures: Object<String, String | Object>,
+    fixtures: Fixture,
+    opts?: FsMock,
     callback: Callback<Error>
 ) => void
 
-teardown-fixtures := (
+fixtures-fs/teardown-fixtures := (
     dirname: String,
-    fixtures: Object<String, String | Object>,
+    fixtures: Fixture,
+    opts?: FsMock,
     callback: Callback<Error>
 ) => void
+
+fixtures-fs :=
+    (
+        dirname: String,
+        fixtures: Fixture, 
+        lambda: (Task<T>) => void,
+        opts?: FsMock,
+        task: Task<T>
+    ) => void &
+    (
+        dirname: String,
+        fixtures: Fixture, 
+        lambda: (Task<T>) => void,
+        opts?: FsMock
+    ) => (Task<T>) => void
+    
